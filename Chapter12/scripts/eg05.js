424ab702c10bf621415f470b8696cd2b07c266cb
wsaicyj <wsaicyj@gmail.com>
2016-08-22 18:34:24 +0800

    chapter12 表单验证4

---
+++ Chapter12/scripts/eg05.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Chen on 2016/8/22.
+ */
+function changeImages(img, imgPath) {
+    img = new Image();
+    img.src = "images/home.jpg";
+    imgPath = new Image();
+    imgPath.src = "images/home1.jpg";
+    /*
+    if (document.images) {
+        for (var i=0;changeImages.arguments.length;i+=2) {
+            document[changeImages.arguments[i]].src = eval(changeImages.arguments[i+1] + ".src")
+        }
+    } */
+    
+}
